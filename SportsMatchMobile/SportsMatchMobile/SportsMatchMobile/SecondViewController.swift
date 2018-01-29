f2ea429b5e3cb21520ee71c3f356e96e5686afe5
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-28 21:24:21 -0600

    Initial commit.

---
+++ SportsMatchMobile/SportsMatchMobile/SportsMatchMobile/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SportsMatchMobile
+//
+//  Created by Christopher Nady on 1/28/18.
+//  Copyright © 2018 Nady Analytics, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
