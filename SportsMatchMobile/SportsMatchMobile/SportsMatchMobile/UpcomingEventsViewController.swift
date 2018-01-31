54b4227a620ed7fcfd7cec0a92542111f20cedfd
ChristopherJamesN <nady.christopher@gmail.com>
2018-01-31 07:24:35 -0600

    Add an upcoming events view controller.

---
+++ SportsMatchMobile/SportsMatchMobile/SportsMatchMobile/UpcomingEventsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  UpcomingEventsViewController.swift
+//  SportsMatchMobile
+//
+//  Created by Christopher Nady on 1/31/18.
+//  Copyright © 2018 Nady Analytics, LLC. All rights reserved.
+//
+
+import UIKit
+
+class UpcomingEventsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
