1dffa62ee8b16f80ebeec0c86ab19b013183b60b
ChristopherJamesN <nady.christopher@gmail.com>
2018-02-04 14:54:29 -0600

    Add call to remote API to get upcoming events.

--- SportsMatchMobile/SportsMatchMobile/SportsMatchMobile/UpcomingEventsViewController.swift
+++ SportsMatchMobile/SportsMatchMobile/SportsMatchMobile/UpcomingEventsViewController.swift
@@ -15,6 +15,43 @@ class UpcomingEventsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         UpcomingEventsLabel.text = event1?.participants
+
+        let session = URLSession.shared
+        
+        let eventsURL = URL(string:"https://sports-match.herokuapp.com/home.json")!
+        
+        let dataTask = session.dataTask(with: eventsURL) {
+            (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let error = error {
+                print("Error:\n\(error)")
+            } else {
+                if let data = data {
+                    
+                    let dataString = String(data: data, encoding: String.Encoding.utf8)
+                    print("All the upcoming event data:\n\(dataString!)")
+                    
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary {
+                        
+                        if let mainDictionary = jsonObj!.value(forKey: "main") as? NSDictionary {
+                            if let particpants = mainDictionary.value(forKey: "participants") {
+                                DispatchQueue.main.async {
+                                    self.UpcomingEventsLabel.text = "\(particpants)"
+                                }
+                            }
+                        } else {
+                            print("Error: unable to find events in dictionary")
+                        }
+                    } else {
+                        print("Error: unable to convert json data")
+                    }
+                } else {
+                    print("Error: did not receive data")
+                }
+            }
+        }
+        
+        dataTask.resume()
         
     }
     
