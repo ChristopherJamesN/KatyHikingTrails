dc7337f707ba7cd7f3e0cedd196a362d46a0946a
ChristopherJamesN <nady.christopher@gmail.com>
2018-02-06 07:23:45 -0600

    Add a table view to your matches view controller.

--- SportsMatchMobile/SportsMatchMobile/SportsMatchMobile/YourMatchesViewController.swift
+++ SportsMatchMobile/SportsMatchMobile/SportsMatchMobile/YourMatchesViewController.swift
@@ -10,23 +10,14 @@ import UIKit
 
 class YourMatchesViewController: UIViewController {
     
+    @IBOutlet weak var YourMatchesTableView: UITableView!
+    @IBOutlet weak var YourMatchesLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        StoredMatchesLabel.text = match1?.description
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    @IBOutlet weak var YourMatchesLabel: UILabel!
-    @IBOutlet weak var StoredMatchesLabel: UILabel!
-    
-    @IBOutlet weak var MatchesTextInput: UITextField!
-    @IBAction func DisaplyMatchesButton(_ sender: Any) {
-        YourMatchesLabel.text = MatchesTextInput.text
     }
     
     let match1 = Match(description: "First match", chance: 15, value: 10, category: "Basketball", event: "Lakers versus Thunder")
