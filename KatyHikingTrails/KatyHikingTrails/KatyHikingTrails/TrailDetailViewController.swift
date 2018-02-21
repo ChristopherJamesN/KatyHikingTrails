a8fd7aec5c734a00f883824ceccbac7c36977834
ChristopherJamesN <nady.christopher@gmail.com>
2018-02-20 21:23:46 -0600

    Add a request for the route between the user and the trail location.

--- KatyHikingTrails/KatyHikingTrails/KatyHikingTrails/TrailDetailViewController.swift
+++ KatyHikingTrails/KatyHikingTrails/KatyHikingTrails/TrailDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class TrailDetailViewController: UIViewController {
+class TrailDetailViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var TrailNameLabel: UILabel!
     @IBOutlet weak var TrailDescriptionTextView: UITextView!
@@ -25,17 +25,41 @@ class TrailDetailViewController: UIViewController {
         TrailNameLabel.text = self.Name
         TrailDescriptionTextView.text = self.Description
         TrailDescriptionTextView.isEditable = false
-    
+        
+        MapView.delegate = self
         MapView.mapType = .standard
         MapView.showsUserLocation = true
         MapView.showsScale = true
         MapView.showsCompass = true
         locationManager.requestWhenInUseAuthorization()
+        let InitialLocation = CLLocationCoordinate2D(latitude: 29.7491301, longitude: -95.7142318)
         
-        let url = NSURL (string: self.MapLink);
-        if url != nil{
-            let requestObj = NSURLRequest(url: url! as URL);
+        let request = MKDirectionsRequest()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: MapView.userLocation.coordinate, addressDictionary: nil))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: InitialLocation, addressDictionary: nil))
+        request.requestsAlternateRoutes = true
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        
+        directions.calculate { [unowned self] response, error in
+            guard let unwrappedResponse = response else { return }
+            
+            for route in unwrappedResponse.routes {
+                self.MapView.add(route.polyline)
+                self.MapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            }
         }
+    }
+    
+    func mapView(_ MapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        MapView.setCenter(userLocation.coordinate, animated: true)
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+        renderer.strokeColor = UIColor.blue
+        return renderer
     }
 
     override func didReceiveMemoryWarning() {
