7ff5fa76df41c8082d5913a21a422f0afca29818
ChristopherJamesN <nady.christopher@gmail.com>
2018-02-23 07:20:21 -0600

    Remove dark theme.

--- KatyHikingTrails/KatyHikingTrails/KatyHikingTrails/AppDelegate.swift
+++ KatyHikingTrails/KatyHikingTrails/KatyHikingTrails/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        ThemeManager.applyTheme(theme: .Dark)
+        //ThemeManager.applyTheme(theme: .Dark)
         return true
     }
 
